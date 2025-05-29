df32d553a5db15d8e8b4c5d0021c55bbfbcea0b8
ahmahmmedd <117048252+ahmahmmedd@users.noreply.github.com>
2025-05-29 14:31:20 +0530

    Delete May29 directory

--- May29/2)Temp_Converter.py
+++
@@ -1,8 +0,0 @@
-def convert_temp(value, unit):
-    if unit== 'C':
-        return (value*9/5)+ 32
-    elif unit== 'F':
-        return (value-32)* 5/9
-
-print(convert_temp(10, 'C'))
-print(convert_temp(32, 'F'))
